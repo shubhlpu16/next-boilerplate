8cd96ba10681b80bdb9f9c779d7376bfaa0d9b4a
Shubham Saxena <shubh.lpu16@gmail.com>
2023-09-30 18:02:40 +0530

    chore: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ChakraProvider } from '@chakra-ui/react'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { theme } from '@/styles/theme'
+
+function MyApp({ Component, pageProps }:AppProps) {
+  return (
+    <ChakraProvider theme={theme}> 
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
+
+export default MyApp;
